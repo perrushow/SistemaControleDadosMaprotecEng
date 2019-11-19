5c33f7bb67ede2c1ae5a6145b801f423fde2d57b
XPerezX <xthepro69@gmail.com>
2019-11-19 20:10:27 -0300

    Finalização do CRUD

--- src/Controller/ListagemControler.php
+++ src/Controller/ListagemControler.php
@@ -16,21 +16,45 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class ListagemControler extends AbstractController
 {
     
     /**
-     * @Route("/listagemagendamentos/", name="agendamentos")
+     * @Route("/listagemagendamentos", name="agendamentos")  
      */
 
-    public function gerenciarConsultas()
+    public function gerenciarConsultas()                    
     {
-        $consultas = $this->getDoctrine()->getRepository(Consulta::class)->findAll(); //pega todas as consultas cadastradas no BD e renderiza na pagina listagem
+        $consultas = $this->getDoctrine()->getRepository(Consulta::class)->findAll(); //pega todas as consultas cadastradas no BD e renderiza na pagina listagem 
         return $this->render('listagem/listagem.html.twig', [
             'consultas' => $consultas,
         ]);
+        
+    }
+
+    
+    /**
+     * @Route("/{id}/show", name="consulta_show", methods={"GET"}) 
+     */
+    public function showConsulta(Consulta $id): Response
+    {                   //Pega o id da consulta e renderiza apenas os campos dela  
+        return $this->render('listagem/show.html.twig', [
+            'consulta' => $id,
+        ]);
+    }
+    
+      /**
+     * @Route("delete/{id}", name="consulta_delete") 
+     */
+    public function deleteConsulta($id)
+    {   //Ao clicar no botão de desmarcar consulta, o controler pegará o id da consulta para deleta esse cadastro 
+        $em = $this->getDoctrine()->getManager();   //Conexão ao gerenciador de entidades 
+        $deleteconsulta = $em->getRepository(Consulta::class)->find($id);  //Buscar a consulta com o id selecionado 
+        $em->remove($deleteconsulta);  // Deletar essa consulta 
+        $em->flush();
+        return $this->redirectToRoute('agendamentos'); //Redirecionar para rota 'agendamentos' 
     }
 
 }
